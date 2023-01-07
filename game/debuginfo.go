0157e67df801380c1576770a3017e87cc1aa84d1
yxqsnz <yxqsnz@gmail.com>
2023-01-07 12:31:22 -0300

    feat: improve debug

--- game/debuginfo.go
+++ game/debuginfo.go
@@ -12,10 +12,11 @@ func DrawDebugInfo(state *State) {
 	if state.IsShowingDebugInfo {
 		mem := runtime.MemStats{}
 		runtime.ReadMemStats(&mem)
-
+		refrate := rl.GetMonitorRefreshRate(rl.GetCurrentMonitor())
+		fps := rl.GetFPS()
 		state.Text.DrawNextLine(fmt.Sprintf("Frame time: %.5f", rl.GetFrameTime()), 20, rl.Brown)
-		state.Text.DrawNextLine(fmt.Sprintf("FPS: %d", rl.GetFPS()), 20, rl.Brown)
-		state.Text.DrawNextLine(fmt.Sprintf("Memory: %s (Frame GC Enabled: %v)", humanize.Bytes(mem.Alloc), state.FrameGC), 20, rl.Brown)
+		state.Text.DrawNextLine(fmt.Sprintf("FPS: %d (%v%%)", fps, (100*fps)/int32(refrate)), 20, rl.Brown)
+		state.Text.DrawNextLine(fmt.Sprintf("Memory (Go): %s (Frame GC Enabled: %v)", humanize.Bytes(mem.Alloc), state.FrameGC), 20, rl.Brown)
 		state.Text.DrawNextLine(fmt.Sprintf("Position (X, Y, Z): %f/%f/%f", state.Camera.Position.X, state.Camera.Position.Y, state.Camera.Position.Z), 20, rl.Brown)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.19
 
 require github.com/gen2brain/raylib-go/raylib v0.0.0-20221204123137-d6b1dea578e9
 
-require github.com/dustin/go-humanize v1.0.0 // indirect
+require github.com/dustin/go-humanize v1.0.0
